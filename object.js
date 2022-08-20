d9f81b8f097d9f7a8c281e95c93b6d01d6090892
wifiseo <wifiseoservice@gmail.com>
2022-08-20 11:41:49 +0600

    add object file

---
+++ object.js
@@ -0,0 +1,14 @@
+const student = [
+    {id: 32, name: 'omar sunn'},
+    {id: 34, name: 'don'},
+    {id: 85, name: 'moto'},
+    {id: 45, name: 'patlo'},
+];
+
+const nameS = student.map(x => x.name);
+const  idS = student.map(x => x.id);
+
+const smaller = student.filter(x => x.id < 40 );
+console.log("the all id is", idS);
+console.log(nameS);
+console.log(smaller);
---
+++ script.js
@@ -0,0 +1,6 @@
+const number = [ 3, 4, 2, 5, 6];
+
+
+
+const squer = number.filter(x => x < 6);
+console.log(squer);
